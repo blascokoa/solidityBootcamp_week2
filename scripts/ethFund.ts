bb1b80b20423310b3097a9171d3fd6944675acfd
Blasco <blascokoa1@gmail.com>
2022-07-03 17:01:24 +0100

    refactor: separate files for ETH fund and ERC20 fund

--- scripts/ethFund.ts
+++ scripts/ethFund.ts
@@ -1,7 +1,5 @@
-import { ethers, Contract } from "ethers";
+import { ethers } from "ethers";
 import "dotenv/config";
-import * as tokenContractJson from "../artifacts/contracts/Token.sol/MyToken.json";
-import { MyToken } from "../typechain";
 
 const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
@@ -45,34 +43,26 @@ async function main() {
     return;
   }
 
-  const tokenContract: MyToken = new Contract(
-    "0x510Dd753824e9035939AE0b7096128d3b1dd9FB7",
-    tokenContractJson.abi,
-    signer
-  ) as MyToken;
+  console.log("- Fund wallets...");
+  const nonce = await signer.getTransactionCount();
+  await Promise.all([
+    signer.sendTransaction({
+      to: wallet2.address,
+      value: ethers.utils.parseEther("0.01"),
+      nonce: nonce,
+    }),
+    signer.sendTransaction({
+      to: wallet3.address,
+      value: ethers.utils.parseEther("0.01"),
+      nonce: nonce + 1,
+    }),
+    signer.sendTransaction({
+      to: wallet4.address,
+      value: ethers.utils.parseEther("0.01"),
+      nonce: nonce + 2,
+    }),
+  ]);
 
-  console.log("- Minting tokens to wallets...");
-
-  const tx1 = await tokenContract.mint(
-    wallet2.address,
-    ethers.utils.parseEther("10")
-  );
-  const receipt1 = await tx1.wait();
-  console.log("- Transaction 1:", receipt1.transactionHash);
-  const tx2 = await tokenContract.mint(
-    wallet3.address,
-    ethers.utils.parseEther("50")
-  );
-  const receipt2 = await tx2.wait();
-  console.log("- Transaction 2:", receipt2.transactionHash);
-  const tx3 = await tokenContract.mint(
-    wallet4.address,
-    ethers.utils.parseEther("33")
-  );
-  const receipt3 = await tx3.wait();
-  console.log("- Transaction 3:", receipt3.transactionHash);
-
-  console.log("Minting transactions mined...");
   process.exit(0);
 }
 
