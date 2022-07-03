09a25126c012f90a76e5dee93d2b1e2909ad512b
stan-dot <stanislawmalinowski09@gmail.com>
2022-07-03 21:22:39 +0100

    made the vote script work for many wallets

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -14,27 +14,54 @@ async function main() {
       : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
   console.log(`Wallet 1: Using address ${wallet.address} (Deployer)`);
 
+  const wallet2 =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC, "m/44'/60'/0'/0/1")
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  console.log(`Wallet 2: Using address ${wallet2.address}`);
+
+  const wallet3 =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC, "m/44'/60'/0'/0/2")
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  console.log(`Wallet 3: Using address ${wallet3.address}`);
+
+  const wallet4 =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC, "m/44'/60'/0'/0/3")
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  console.log(`Wallet 3: Using address ${wallet4.address}`);
+
   const provider = ethers.providers.getDefaultProvider("ropsten", {
     etherscan: process.env.ETHERSCAN_API_KEY,
     infura: process.env.INFURA_API_KEY,
   });
 
-  const ballotContractAddress = process.argv.slice(2)[0];
+  const ballotContractAddress = process.argv.slice(4)[0];
   if (!ballotContractAddress.startsWith("0x")) {
     throw new Error("Invalid ballot contract address");
   }
 
-  const voter = wallet.connect(provider);
+  // eslint-disable-next-line no-unused-vars
+  const signer = wallet.connect(provider);
+  const signer2: ethers.Wallet = wallet2.connect(provider);
+  const signer3: ethers.Wallet = wallet3.connect(provider);
+  const signer4: ethers.Wallet = wallet4.connect(provider);
 
-  const contract: CustomBallot = new ethers.Contract(
-    ballotContractAddress,
-    ballotContractJson.abi,
-    voter
-  ) as CustomBallot;
-  const proposalNumber: string = process.argv.slice(3)[0];
-  const power = await contract.votingPower();
-  contract.vote(proposalNumber, power);
+  const voters = [signer2, signer3, signer4];
 
+  let readArgCounter = 1;
+  for (const voter of voters) {
+    const contract: CustomBallot = new ethers.Contract(
+      ballotContractAddress,
+      ballotContractJson.abi,
+      voter
+    ) as CustomBallot;
+    const proposalNumber: string = process.argv.slice(readArgCounter)[0];
+    readArgCounter++;
+    const power = await contract.votingPower();
+    contract.vote(proposalNumber, power);
+  }
   process.exit(0);
 }
 
